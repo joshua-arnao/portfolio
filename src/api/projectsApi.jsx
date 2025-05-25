9af89f8afc41dba16a259a93499ad077f185f859
Joshua Arnao Canessa <joshuaarnaocanessa@Joshuas-MacBook-M3.local>
2025-05-24 20:44:49 -0500

    update styles and add lamda JSON projects

---
+++ src/api/projectsApi.jsx
@@ -0,0 +1,25 @@
+const BASE_URL = import.meta.env.VITE_API_PROJECTS;
+
+export const getProjects = async () => {
+  try {
+    const response = await fetch(BASE_URL);
+    if (!response.ok) throw new Error("Error al obtener los proyectos");
+    return await response.json();
+  } catch (error) {
+    console.error("Error en getProjects:", error);
+    return [];
+  }
+};
+
+export const getProjectById = async (id)=>{
+    try {
+        const response = await fetch(`${BASE_URL}/${id}`)
+        if(!response.ok){
+            throw new Error(`Error en la API ${response.status}`)
+        }
+        return await response.json()
+    } catch (error) {
+        console.error('Error en getProjectById:', error);
+        return null
+    }
+}
--- src/portfolio/views/ProjectsView.jsx
+++ src/portfolio/views/ProjectsView.jsx
@@ -1,37 +1,28 @@
+import { useProjects } from "../../hook/useProjects";
 import { Container, Typography } from "@mui/material";
 import { CardProject } from "../components/CardProject";
 import { Link } from "react-router-dom";
 import { useIsSmallScreen } from "/src/hook/useSmallScreen";
-
-const projects = [
-  {
-    projectId: 1,
-    title: "Rediseño rentadora de auto | App",
-    description:
-      "Redesigning tiket.coms Car Rentals mobile app due to high inquiries regarding the product & service",
-    imgCard: "/assets/photos/PreviewRentadora.png",
-    tools: ["Figma"],
-  },
-  {
-    projectId: 2,
-    title: "Proceso de Créditos | Web",
-    description:
-      "Este proyecto buscaba desarrollar una plataforma intermediaria entre clientes y bancos en Panamá...",
-    imgCard: "/assets/photos/HomeBankiao.png",
-    tools: ["React", "Marvel", "Figma"],
-  },
-  {
-    projectId: 3,
-    title: "Figma Tracking Analytics | Widget",
-    description:
-      "Contribuir a la comunidad de Figma al crear un widget que facilita la documentación y el seguimiento de etiquetas...",
-    imgCard: "/assets/photos/PreviewWidget.png",
-    tools: ["TypeScript", "Figma", "Google Analytics"],
-  },
-];
+import { Box } from "@mui/system";
+import { HashLoader } from "react-spinners";
 
 export const ProjectsView = ({ currentTheme }) => {
+  const { projects, loading, error } = useProjects();
   const isSmallScreen = useIsSmallScreen();
+  const {secundary} = currentTheme.palette
+
+  if (loading)
+    return (
+      <Box
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap:"16px" }}
+      >
+        <HashLoader size={40} color={secundary.main}/>
+        <Typography>Cargando...</Typography>
+      </Box>
+    );
+  if (error) return <Typography>Error: {error}</Typography>;
+
+  console.log(projects);
 
   return (
     <Container
@@ -52,8 +43,8 @@ export const ProjectsView = ({ currentTheme }) => {
 
       {projects.map((project) => (
         <Link
-          key={project.projectId}
-          to={`/projects/${project.projectId}`}
+          key={project.id}
+          to={`/projects/${project.id}`}
           style={{ textDecoration: "none" }}
         >
           <CardProject
